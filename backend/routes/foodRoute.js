3b2dff3ddb613d8b808605d3cd594c9f8d29c0a3
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-06-10 12:49:15 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,30 +1,21 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
-import path from 'path';
-import os from 'os';
-import mkdirp from 'mkdirp';
-
 const foodRouter = express.Router();
 
-// Ensure the upload directory exists
-const uploadDir = path.join(os.tmpdir(), 'uploads'); // Updated path
-mkdirp.sync(uploadDir); // Create the uploads directory if it doesn't exist
+//Image Storage Engine (Saving Image to uploads folder & rename it)
 
-// Image Storage Engine (Saving Image to /tmp/uploads folder & rename it)
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
+    destination: 'uploads',
     filename: (req, file, cb) => {
-        cb(null, `${Date.now()}${file.originalname}`);
+        return cb(null,`${Date.now()}${file.originalname}`);
     }
-});
+})
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage})
 
-foodRouter.get("/list", listFood);
-foodRouter.post("/add", upload.single('image'), addFood);
-foodRouter.post("/remove", removeFood);
+foodRouter.get("/list",listFood);
+foodRouter.post("/add",upload.single('image'),addFood);
+foodRouter.post("/remove",removeFood);
 
 export default foodRouter;
